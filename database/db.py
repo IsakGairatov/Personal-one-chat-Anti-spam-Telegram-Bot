b4b28d016da7aae62ec3ddebfe97324228536169
gairat <isak3141592653@gmail.com>
2026-08-17 17:53:20 +0500

    Alembic added

--- database/db.py
+++ database/db.py
@@ -6,9 +6,6 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
 )
 
-from database.models import Base
-
-
 BASE_DIR = Path(__file__).resolve().parent
 DATABASE_PATH = BASE_DIR / "bot.db"
 
@@ -26,7 +23,3 @@ async_session = async_sessionmaker(
     class_=AsyncSession,
     expire_on_commit=False,
 )
-
-async def create_tables():
-    async with engine.begin() as connection:
-        await connection.run_sync(Base.metadata.create_all)
